119e871b1ff8da73255a175b76ef154938eb0cf4
dark-mais <dark1mais@gmail.com>
2023-12-10 11:19:04 +0300

    сделал ДЗ10

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
     }
+
+    private static void task1() {
+        System.out.println("Задача №1.");
+        String firstName = "Aleksey";
+        String middleName = "Sergeevich";
+        String lastNAme = "Mostolygin";
+        String fullName = lastNAme + " " + firstName + " " + middleName;
+        System.out.println(fullName);
+    }
+    private static void task2() {
+        System.out.println("Задача №2.");
+        String firstName = "Aleksey";
+        String middleName = "Sergeevich";
+        String lastNAme = "Mostolygin";
+        String fullName = lastNAme + " " + firstName + " " + middleName;
+        System.out.println(fullName.toUpperCase());
+    }
+    private static void task3() {
+        System.out.println("Задача №3.");
+        String firstName = "ЁСемён";
+        String middleName = "Семёнович";
+        String lastNAme = "Иванов";
+        String fullName = lastNAme + " " + firstName + " " + middleName;
+        fullName = fullName.replace("ё", "е");
+        fullName = fullName.replace("Ё", "Е");
+        System.out.println(fullName);
+        System.out.println("как написать проще замена Ё, ё на Е, е?");
+    }
+
 }
